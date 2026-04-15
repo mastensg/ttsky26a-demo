40f09273ce6615c3fb443d2f894814a0d1d49557
Martin Stensgård <mastensg@mastensg.net>
2026-04-15 19:56:33 +0200

    tt_um_mastensg_ttsky26a_demo

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2026 Martin Stensgård
+ * SPDX-License-Identifier: ISC
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mastensg_ttsky26a_demo (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
