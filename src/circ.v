705992fc476e2d48ea34a7f9a46801b3a76447ba
Martin Stensgård <mastensg@mastensg.net>
2026-05-11 21:05:47 +0200

    src/circ: be x

--- src/circ.v
+++ src/circ.v
@@ -9,11 +9,11 @@ module circ(
 	input	wire	run,
 	input	wire [7:0] in
 );
-	reg [9:0] rx;
-	reg [9:0] ry;
-	reg [1:16] x;
-	reg signed [10:0] cx = 320;
-	reg signed [10:0] cy = 240;
+	reg [19:0] rx;
+	reg [19:0] ry;
+	reg [15:0] x;
+	reg signed [19:0] cx = 320;
+	reg signed [19:0] cy = 240;
 	reg [19:0] s2 = 128*128;
 
 	wire signed [19:0] a = rx-cx;
@@ -29,7 +29,7 @@ module circ(
 			ry <= 0;
 		end else begin
 			if (rx[1:0] == 0)
-				x <= {x[11] ^ x[13] ^ x[14] ^ x[16], x[1:15]};
+				x <= {x[5] ^ x[3] ^ x[2] ^ x[0], x[15:1]};
 			if ((640+16 <= rx) && (rx < 640+16+96))	H <= 0;
 			else					H <= 1;
 			if ((480+11 <= ry) && (ry < 480+11+2))	V <= 0;
@@ -37,7 +37,7 @@ module circ(
 			if (rx < 640+16+96+48-1) begin
 				rx <= rx+1;
 			end else begin
-				A <= x[1];
+				A <= x[0];
 				rx <= 0;
 				if (ry < 480+11+2+31-1) begin
 					ry <= ry+1;
@@ -46,9 +46,9 @@ module circ(
 				end
 			end
 			if (r2 < s2) begin
-				R <= x[1:3];
-				G <= x[1:3];
-				B <= x[1:3];
+				R <= x[3:2];
+				G <= x[3:2];
+				B <= x[3:2];
 			end else begin
 				R <= 0;
 				G <= 0;
